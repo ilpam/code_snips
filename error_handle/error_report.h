1a17c46dd767ed99d6c13239cebe6e78c4e749ba
metalmoon <tan.dejia@foxmail.com>
2020-11-06 16:21:32 +0800

    delete files

--- error_handle/error_report.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ERROR_REPORT_H_
-#define _ERROR_REPORT_H_
-
-void unix_error(char *msg);
-void posix_error(int code, char *msg);
-void gai_error(int code, char *msg);
-void app_error(char *msg);
-
-#endif
--- network/openfd/lib/openfd.h
+++
@@ -1,8 +0,0 @@
-#ifndef _OPENFD_H_
-#define _OPENFD_H_
-
-#define LISTENQ  1024  /* Second argument to listen() */
-int open_clientfd(char *hostname, char *port);
-int open_listenfd(char *port);
-
-#endif
